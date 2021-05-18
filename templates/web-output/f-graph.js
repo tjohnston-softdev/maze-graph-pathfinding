74fd22b0aa029700eb62670a36957b2d46692669
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-05-18 15:25:36 +1000

    Basic Cytoscape Styling
    Started experimenting with styling for
    Cytoscape graph objects.
    
    * Node colour and shape
    * Border colour and width.
    * Node label text and alignment

--- templates/web-output/f-graph.js
+++ templates/web-output/f-graph.js
@@ -3,4 +3,29 @@
 			{
 				container: document.getElementById("graphContainer"),
 				elements: {nodes: nodeContents, edges: edgeContents},
+				style:
+				[
+					{
+						selector: "node",
+						style:
+						{
+							"shape": "circle",
+							"background-color": "#7EA6E0",
+							"border-color": "#000000",
+							"border-width": 1,
+							"label": "data(id)",
+							"text-valign": "center",
+							"text-halign": "center"
+						
+						}
+					},
+					{
+						selector: "edge",
+						style:
+						{
+							"line-color": "#000000",
+							"line-width": 1
+						}
+					}
+				]
 			});
